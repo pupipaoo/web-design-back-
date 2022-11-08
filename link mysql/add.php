723a9316b4c6d973351a68b9c542b94073b010f5
pupipaoo <114714786+pupipaoo@users.noreply.github.com>
2022-11-08 17:53:14 +0800

    111.11.8 web-design-back-

---
+++ link mysql/add.php
@@ -0,0 +1,10 @@
+<html>
+	<meta charset="utf-8">
+	<form action="add.php" method="GET">
+		帳號<input type="text" name="ACCOUNT" value=""><br>
+		密碼<input type="text" name="PASSWORD" value=""><br>
+		姓名<input type="text" name="NAME" value=""><br>
+		信箱<input type="text" name="MAIL" value=""><br>
+		<input type="submit" value="送出"><br>
+	</form>
+</html>
---
+++ link mysql/update.php
@@ -0,0 +1,16 @@
+<?php
+	print_r($_GET);
+	$link=mysqli_connect("127.0.0.1","root","","user") or die("無法連接資料庫:".mysql_error());
+	
+	$query="update test set 
+	ACCOUNT='".$_GET["ACCOUNT"]."',
+	PASSWORD='".$_GET["PASSWORD"]."',
+	NAME='".$_GET["NAME"]."',
+	MAIL='".$_GET["MAIL"]."'
+	where ID ='".$_GET["id"]."'";
+	echo $query;
+	
+	$result=mysqli_query($link,$query)or die("無法送出".mysql_error());
+	echo"<script>alert('山成功');</script>";
+	echo"<script>location.href='list.php'</script>";
+?>
