c716d4890a5dbebabfd7eec914362d9447f85b73
pupipaoo <114714786+pupipaoo@users.noreply.github.com>
2022-11-12 12:26:07 +0800

    Delete delete.php

--- link mysql/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-	print_r($_GET);
-	$link=mysqli_connect("127.0.0.1","root","","user") or die("無法連接資料庫:".mysql_error());
-	$query="delete from test where ID ='".$_GET["id"]."'";
-	$result=mysqli_query($link,$query)or die("無法送出".mysql_error());
-	echo"<script>alert('山成功');</script>";
-	echo"<script>location.href='list.php'</script>";
-?>
