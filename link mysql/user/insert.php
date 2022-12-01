c7fa3fc45f009bbffbcf8935c3db260a47d0de9e
pupipaoo <114714786+pupipaoo@users.noreply.github.com>
2022-12-01 16:56:40 +0800

    111.11.12 web-design-back-

--- link mysql/user/insert.php
+++ link mysql/user/insert.php
@@ -1,6 +1,6 @@
 <html>
 	<meta charset="utf-8">
-	<form action="add.php" method="GET">
+	<form action="add.php" method="GET">	//action的意思是，當表單提交後，送到何處
 		帳號<input type="text" name="ACCOUNT" value=""><br>
 		密碼<input type="text" name="PASSWORD" value=""><br>
 		姓名<input type="text" name="NAME" value=""><br>
